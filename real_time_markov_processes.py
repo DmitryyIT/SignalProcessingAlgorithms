8030ca1eb4025460d6bf7880e4dcfed71ffcd514
AI Tech <132939290+DmitryyIT@users.noreply.github.com>
2024-02-19 18:42:40 +0200

    Update real_time_markov_processes.py

--- real_time_markov_processes.py
+++ real_time_markov_processes.py
@@ -2,15 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 N = 1000
-sigma = 1
+sigma = 5
+r = 0.99 # Чтобы не было бесконечного прироста диспресии
+en = np.sqrt((1 - r ** 2) * sigma ** 2)
 
 fSignal = np.zeros(N)
-fNoise = np.random.normal(0, sigma, N)
+fSignal[0] = np.random.normal(0, sigma)
 
 for i in range(N):
-    fSignal[i] = fSignal[i-1] + fNoise[i]
+    fSignal[i] = r * fSignal[i-1] + np.random.normal(0, en)
 
 plt.plot(fSignal)
-plt.plot(fNoise)
 plt.grid(True)
 plt.show()
